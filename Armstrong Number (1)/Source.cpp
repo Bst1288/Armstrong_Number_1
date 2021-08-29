9edd09e019550cba0c115cafddded4120b9eb573
Bst1288 <88548302+Bst1288@users.noreply.github.com>
2021-08-29 20:20:06 +0700

    Add project files.

---
+++ Armstrong Number (1)/Source.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num, x, r, e = 0, sum = 0;
+
+    printf("Enter number : ");
+    scanf_s("%d", &num);
+
+    x = num;
+    while (x != 0)
+    {
+        e = e + 1;
+        x = x / 10;
+    }
+
+    x = num;
+    while (x > 0)
+    {
+        r = x % 10;
+        sum = sum + pow(r, e);
+        x = x / 10;
+    }
+
+    printf("sum = %d\n", sum);
+
+    if (num == sum)
+        printf("\n%d is Armstrong Number\n", num);
+    else
+        printf("\n%d isn't a Armstrong Number\n", num);
+
+    return 0;
+}
